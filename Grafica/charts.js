dbea021de80634bf1050229396e0ffb7fdc0a288
Noel Fabello Quintana <noelfabello@gmail.com>
2022-09-28 20:14:32 +0200

    Analisis casi hecho
    Co-authored-by: antonc744@outlook.es <antonc744@outlook.es>

--- Grafica/charts.js
+++ Grafica/charts.js
@@ -1,5 +1,6 @@
 window.docReady(() => {
     createAndAddLineChart();
+    createAndAddPieChart();
 });
 
 function createAndAddLineChart() {
@@ -7,7 +8,7 @@ function createAndAddLineChart() {
     let mesesIntervalo = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
     let datos = [{
         name: 'Comunicaciones',
-        data: [100, 200, 300, 150, 235, 254, 345, 293, 392, 124, 102, 134]
+        data: [50, 200, 120, 150, 235, 254, 250, 293, 280, 124, 102, 134]
     },
     {
         name: 'Ocio',
@@ -15,21 +16,21 @@ function createAndAddLineChart() {
     },
     {
         name: 'Alimentación',
-        data: [155, 132, 158, 156, 160, 135, 134, 129, 139, 112, 110, 113]
+        data: [155, 132, 158, 156, 250, 135, 134, 129, 139, 112, 110, 113]
     },
     {
         name: 'Combustible',
-        data: [50, 55, 60, 65, 70, 75, 80, 0, 170, 80, 85, 82]
+        data: [50, 55, 60, 65, 70, 75, 80, 100, 170, 80, 85, 82]
     },
     {
-        name: 'Suministros',
+        name: 'Otros',
         data: [45, 45, 49, 50, 60, 65, 62, 66, 62, 66, 70, 65]
     }];
 
     // Configure and put the chart in the Html document
-    Highcharts.chart('container', {
+    Highcharts.chart('containerLinea', {
         title: {
-            text: 'Gastos en los últimos doce meses'
+            text: ''
         },
 
 
@@ -79,4 +80,57 @@ function createAndAddLineChart() {
         }
 
     });
+}
+
+function createAndAddPieChart() {
+    Highcharts.chart('containerTarta', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: ''
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: [{
+                name: 'Comunicaciones',
+                y: 134/407,
+            },  {
+                name: 'Ocio',
+                y: 13/407
+            },  {
+                name: 'Alimentación',
+                y: 113/407
+            }, {
+                name: 'Combustible',
+                y: 82/407
+            }, {
+                name: 'Otros',
+                y: 65/407
+            }]
+        }]
+    });
+    
 }
